f6455d956b74c5577b9e11a337384f3b262801df
anishazahan <anishazahan13@gmail.com>
2022-12-15 00:24:24 +0600

    cateogory card section responsive

--- src/Pages/Home/CateogoryCard.js
+++ src/Pages/Home/CateogoryCard.js
@@ -44,11 +44,11 @@ const CateogoryCard = () => {
 
   return (
     <div className='max-w-7xl mx-auto px-16 pt-8'>
-        <div className="flex w-full mx-auto">
+        <div className="flex flex-col md:flex-row w-full mx-auto">
 
             {
                 cateogoryCard.map((item,index)=>{
-                    return <div className="bg-gray-100 border border-r flex flex-col py-5 px-6 space-y-6 justify-center items-center w-[17%]">
+                    return <div className="bg-gray-100 border border-r flex flex-col py-5 px-6 space-y-6 justify-center items-center w-full md:w-[17%]">
                                 <h2 className='text-[18px]'>{item.name}</h2>
                                 <button className='text-5xl text-gray-800'><FaLaptopCode></FaLaptopCode> </button> 
                                 <p>{item.position} Open Position</p>
