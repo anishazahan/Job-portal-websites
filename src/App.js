d1bf69836c836cd6c26001c84f6fcf235553cbfc
anishazahan <anishazahan13@gmail.com>
2022-12-14 21:59:52 +0600

    select input field add in hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Shared/Header/Header'
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <>
    <Header></Header>
+    <Routes>
+    <Route path='/' element={<Home></Home>}> </Route>
+    </Routes>
 
   
 
--- src/Pages/Home/Hero.js
+++ src/Pages/Home/Hero.js
@@ -1,16 +1,70 @@
 import React from 'react'
+import { FaBriefcase } from "react-icons/fa";
 
 const Hero = () => {
   return (
-    <>
+    <div className='bg-[#192A56] py-20 '>
 
-    <div className="hero-section bg-[#192A56] py-10  max-w-7xl mx-auto">
+    <div className="hero-section max-w-7xl mx-auto">
 
+        <div className="">
+            <h2 className='text-center text-white py-5 '>Find Job, Employment, and Career Opportunities</h2>
+            <h2 className='text-center text-3xl text-white pb-10 font-medium'>The Eassiest Way to Get Your New Job</h2>
+
+        </div>
+
+        <div className="">
+            <form className='bg-gray-200 py-8 px-5 mx-16'>
+
+            <div class="flex justify-center space-x-5">
+
+            <div class="relative inline-block w-full text-gray-700 rounded border border-gray-400">
+            <button className='absolute flex items-center p-3 mr-3 bg-gray-200'><FaBriefcase></FaBriefcase></button>
+            <select class="w-full h-10 pl-10 pr-6 text-base placeholder-gray-600  rounded appearance-none focus:shadow-lg outline-none" placeholder="Regular input">
+                <option>A regular sized select </option>
+                <option>Another option</option>
+                <option>And one more</option>
+            </select>
+            <div class="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+                <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20"><path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
+            </div>
+            </div>
+            <div class="relative inline-block w-full text-gray-700 rounded border border-gray-400">
+            <button className='absolute flex items-center p-3 mr-3 bg-gray-200'><FaBriefcase></FaBriefcase></button>
+            <select class="w-full h-10 pl-10 pr-6 text-base placeholder-gray-600  rounded appearance-none focus:shadow-lg outline-none" placeholder="Regular input">
+                <option>A regular sized select </option>
+                <option>Another option</option>
+                <option>And one more</option>
+            </select>
+            <div class="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+                <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20"><path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
+            </div>
+            </div>
+            <div class="relative inline-block w-full text-gray-700 rounded border border-gray-400">
+            <button className='absolute flex items-center p-3 mr-3 bg-gray-200'><FaBriefcase></FaBriefcase></button>
+            <select class="w-full h-10 pl-10 pr-6 text-base placeholder-gray-600  rounded appearance-none focus:shadow-lg outline-none" placeholder="Regular input">
+                <option>A regular sized select </option>
+                <option>Another option</option>
+                <option>And one more</option>
+            </select>
+            <div class="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+                <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20"><path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
+            </div>
+            </div>
+
+            <div className="">
+                <button className='py-2 px-10 border border-gray-400 rounded'>Search</button>
+            </div>
+      </div>
+
+
+            </form>
+        </div>
     </div>
 
 
 
-    </>
+    </div>
   )
 }
 
