f14c0a5830a9872cf4bcb20d6591c6f662d3595b
anishazahan <anishazahan13@gmail.com>
2022-12-14 19:22:04 +0600

    header component create

---
+++ src/Components/Shared/Header.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+
+
+const Menubar = () => {
+    const [nav, setNav] = useState(false);
+    const [navbar, setNavbar] = useState(false);
+  return (
+    <>
+   
+
+          <nav className="w-full bg-[#1D1D1D] shadow">
+            <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+                <div>
+                    <div className="flex items-center justify-between md:py-2 md:block">
+                        <Link to='/'>
+                      Logo
+                        </Link>
+                        <div className="md:hidden">
+                            <button
+                                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                                onClick={() => setNavbar(!navbar)}
+                            >
+                                {navbar ? (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6 text-white"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6 text-white"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        strokeWidth={2}
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M4 6h16M4 12h16M4 18h16"
+                                        />
+                                    </svg>
+                                )}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div
+                        className={`flex-1 justify-center items-center pl-10 md:pl-0 pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                            navbar ? "block" : "hidden"
+                        }`}
+                    >
+                        <ul className="items-center w-full  justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                            <li className="text-white hover:text-emerald-200 font-medium">
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li className="text-white hover:text-emerald-200 font-medium">
+                                <Link to="about">About</Link>
+                            </li>
+                            <li className="text-white hover:text-emerald-200 font-medium">
+                                <Link to="service">Services</Link>
+                            </li>
+                            <li className="text-white hover:text-emerald-200 font-medium">
+                                <Link to="portfolio">Portfolio</Link>
+                            </li>
+                            <li className="text-white hover:text-emerald-200 font-medium">
+                                <Link to="blog">Blogs</Link>
+                            </li>
+                            <li className="text-white hover:text-emerald-200 font-medium">
+                                <Link to="contact">Contact</Link>
+                            </li>
+                        </ul>
+
+                       
+                    </div>
+                </div>
+               
+            </div>
+        </nav>
+
+
+
+    </>
+  )
+}
+
+export default Menubar
