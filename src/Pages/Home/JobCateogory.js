6aa8d5b6258623c6b569e1fc00affaa0d2ff2e9f
anishazahan <anishazahan13@gmail.com>
2022-12-15 14:11:10 +0600

    job cateogory add through mapping

--- src/Pages/Home/JobCateogory.js
+++ src/Pages/Home/JobCateogory.js
@@ -43,13 +43,13 @@ const JobCateogory = () => {
         <h2 className='text-center text-3xl text-black pb-10 font-medium'>Top Categories</h2>
 
 
-        <div className="flex">
+        <div className="grid grid-cols-1 md:grid-cols-3 max-w-7xl mx-auto px-10 lg:px-20 gap-3">
 
         {
                 cateogoryCard.map((item,index)=>{
-                    return <div className="bg-gray-100 border border-r flex flex-col py-5 px-6 space-y-6 justify-center items-center w-full md:w-[17%]">
+                    return <div className="bg-gray-100 border border-r flex flex-col py-5 px-6 space-y-6 justify-center items-center w-full">
                                 <h2 className='text-[18px]'>{item.name}</h2>
-                                <button className='text-5xl text-gray-800'><FaLaptopCode></FaLaptopCode> </button> 
+                                <button className='text-5xl bg-[#192A56] rounded-full p-3 text-white'>{item.icon} </button> 
                                 <p>{item.position} Open Position</p>
                           </div>
                 })
