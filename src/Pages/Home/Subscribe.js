7823046b12788df46e1cc521a34981bc2a317c3b
anishazahan <anishazahan13@gmail.com>
2022-12-15 22:34:34 +0600

    subscribe component create

--- src/Pages/Home/Subscribe.js
+++ src/Pages/Home/Subscribe.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <div className='subscribe-bg'>
+        <div className="">
+            <h2>Subscribe to our job alert</h2>
+            <p>If you want to get email for every posted job. please enter your email here. we will send email to you for every verified job</p>
+        </div>
+
+    </div>
   )
 }
 
